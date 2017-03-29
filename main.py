3a44321749ab1599e4eb8655edb30a9f295b7c00
Radomir Krawczykiewicz <kotwic4@gmail.com>
2017-03-29 15:29:23 +0200

    create MidiHelper

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 from miditime.miditime import MIDITime
 
-# Instantiate the class with a tempo (120bpm is the default) and an output file destination.
-mymidi = MIDITime(240, 'myfile.mid')
 
-# Create a list of notes. Each note is a list: [time, pitch, velocity, duration]
-midi_notes = [
-    [0, 60, 127, 4],   # C4
-    [4, 62, 127, 4],   # D4
-    [8, 64, 127, 4],   # E4
-    [12, 65, 127, 4],  # F4
-    [16, 67, 127, 4],  # G4
-    [20, 69, 127, 4],  # A4
-    [24, 71, 127, 4],  # B4
-    [28, 72, 127, 4],  # C5
-]
+class MidiHelper:
 
-# Add a track with those notes
-mymidi.add_track(midi_notes)
+    pitch_map = {'C4': 60, 'D4': 62, 'E4': 64, 'F4': 65, 'G4': 67, 'A4': 69, 'B4': 71, 'C5': 72}
 
-# Output the .mid file
-mymidi.save_midi()
+    def __init__(self, filename='myfile.mid', bpm=120):
+        # Instantiate the class with a tempo (120bpm is the default) and an output file destination.
+        self.my_midi = MIDITime(bpm, filename)
+        # Create a list of notes. Each note is a list: [time, pitch, velocity, duration]
+        self.midi_notes = []
+        self.i = 0
+
+    def add_pith(self, pitch_b, d=4, volume=127):
+        self.midi_notes.append([self.i, self.pitch_map[pitch_b], volume, d])
+        self.i += d
+
+    def save(self):
+        # Add a track with those notes
+        self.my_midi.add_track(self.midi_notes)
+        # Output the .mid file
+        self.my_midi.save_midi()
+
+midi_helper = MidiHelper()
+pitch_list = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
+
+
+for pitch in pitch_list:
+    midi_helper.add_pith(pitch)
+
+for pitch in reversed(pitch_list):
+    midi_helper.add_pith(pitch)
+
+midi_helper.save()
+
+
+
